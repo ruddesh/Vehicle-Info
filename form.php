a601c7f15ba51b35aff325edc5c539df5a6ffa38
ruddesh <62232025+ruddesh@users.noreply.github.com>
2020-07-07 13:22:40 +0530

    Update form.php

--- form.php
+++ form.php
@@ -6,11 +6,13 @@
 
 <?php  require_once('init.php'); 
 
-if (isset($_POST['nihao'])) 
+if (isset($_POST['VehVar'])) 
 {
-	$name = $_POST['nihao'];		
-	if (!empty($name)) {
+	$check = $_POST['VehVar'];		
+	if (!empty($check)) //to check if user have not submitted empty field
+	{
 		foreach ($vehNums as $vehNum) {
+			// $vehNums from object.php 
 			foreach ($vehNum as $key => $value) {	
 				if (strpos($value, $name) !== false) {
 				 $Id = $vehNum['id'];
@@ -26,8 +28,8 @@ if (isset($_POST['nihao']))
 					echo "<hr>";			
 					echo "<strong>Area Code</strong> :".$Po;
 					echo "<hr>";		
-
-					print("<form action='pdf.php' id='submitHiddenVar' method='post'>
+					// sending the information to pdf.php through hidden fields
+					print("<form action='pdf.php' id='submitHiddenVar' method='post'> 
 					        <input type='hidden' name='HId' value='".$Id."'> 
 					        <input type='hidden' name='HVno' value='".$Vno."'> 
 					        <input type='hidden' name='HOwner' value='".$Owner."'> 
